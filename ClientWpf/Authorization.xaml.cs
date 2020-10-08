4773a77e2271621e1e8154b959dfad52699f7b43
Dzimka <exordzimka@gmail.com>
2020-10-08 13:14:15 +0300

    Client first

---
+++ ClientWpf/Authorization.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ClientWpf
+{
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+        
+        private void Auth_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
